f8c14be9a9e47c923595ab943b41744a677f158a
Dancing-in-air <513029b001g.cdb@sina.cn>
2019-11-16 17:34:03 +0800

    201911161734

--- JdBook/pipelines.py
+++ JdBook/pipelines.py
@@ -6,10 +6,15 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from pprint import pprint
 import re
+from pymongo import MongoClient
 
 
 class JdbookPipeline(object):
+    def open_spider(self, spider):
+        self.collection = MongoClient()["JdBook"]["books"]
+
     def process_item(self, item, spider):
         # 去除空白符
         item["book_name"] = re.sub(r"\s", "", item["book_name"])
+        self.collection.insert(item)
         pprint(item)
